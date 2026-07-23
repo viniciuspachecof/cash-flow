28a52aff6770077dab1b281a396757d6fdf839e7
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-07-22 22:01:36 -0300

    Preparando setup do projeto, validação e tratando erros

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,36 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+       try
+        {
+            var useCase = new RegisterExpenseUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            var errorResponse = new ResponseErrorJson(ex.Message);            
+
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson("unknown error");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+        }
+    }
+}
