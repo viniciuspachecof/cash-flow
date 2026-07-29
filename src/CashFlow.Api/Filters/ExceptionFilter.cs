94b3ba5ca776bf25208ce148b94d57272145a312
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-07-28 22:05:45 -0300

    Localizando as mensagens

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -41,7 +42,7 @@ public class ExceptionFilter : IExceptionFilter
 
     private void ThrowUnkowError(ExceptionContext context)
     {
-        var errorResponse = new ResponseErrorJson("unknown error");
+        var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
 
         context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
         context.Result = new ObjectResult(errorResponse);        
